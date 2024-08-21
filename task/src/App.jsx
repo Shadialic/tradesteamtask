596999e678fa404ccef34b149d5cbf7db06837b5
shadil <shadilsa786@gmai.com>
2024-08-22 00:35:22 +0530

    first commit

---
+++ task/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+import Checking from './componets/Cheking'
+
+
+function App() {
+  
+  return (
+    <>
+    <Checking/>
+{/* <h1>sss</h1> */}
+
+
+     
+   
+       
+    </>
+  )
+}
+
+export default App
